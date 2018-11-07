4667cbe56381d027874b1427c70ddd8a4a17fba6
Khoi <ktranman@yahoo.com>
2018-11-07 09:59:46 -0500

    artist Class built

---
+++ src/main/java/org/wecancodeit/AlbumCollection/Artist/Artist.java
@@ -0,0 +1,42 @@
+package org.wecancodeit.AlbumCollection.Artist;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Artist {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private String imageURL;
+	private Collection<Albums> albums;
+
+	public Artist(String name, String imageURL, Collection<Albums> albums) {
+
+		this.name = name;
+		this.imageURL = imageURL;
+		this.albums = albums;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public Collection<Albums> getAlbums() {
+		return albums;
+	}
+
+}
